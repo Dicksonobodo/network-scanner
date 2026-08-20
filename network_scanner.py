2546b3e3d452f4d10309dc98b1641cfb6d30e963
Dicksonobodo <obododickson7@email.com>
2026-08-20 16:33:51 -0500

    new features

--- network_scanner.py
+++ network_scanner.py
@@ -1,16 +1,31 @@
 #!usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target IP address /IP range")
+    (options, args) = parser.parse_args()
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list= scapy.srp(arp_request_broadcast, timeout=1)[0]
+    answered_list= scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+    clients_list =[]
     for element in answered_list:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-        print("-------------------------------------------------------------------------")
+        client_dict = {"ip":element[1].psrc, "mac":element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
-scan('192.168.180.1/24')
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n----------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+scan_result = scan('192.168.180.1/24')
+print_result(scan_result)
